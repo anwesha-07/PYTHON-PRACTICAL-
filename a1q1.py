d31988230a7ec489e94214d3a46b3d209202f92d
Anwesha Saha <162950224+anwesha-07@users.noreply.github.com>
2025-11-17 22:57:10 +0530

    ASSIGNMENT 1

---
+++ a1q1.py
@@ -0,0 +1,11 @@
+n1=int(input("Enter mark 1:"))
+n2=int(input("Enter mark 2:"))
+n3=int(input("Enter mark 3:"))
+n4=int(input("Enter mark 4:"))
+n5=int(input("Enter mark 5:"))
+
+total=n1+n2+n3+n4+n5
+percntage=(total/500)*100
+
+print("Total Marks=",total)
+print("Percentage=",percntage)
